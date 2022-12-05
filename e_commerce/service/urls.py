38a5582b52e2890864ee5d8f9ef34e885dff9e81
HpT2 <80456375+HpT2@users.noreply.github.com>
2022-12-05 14:02:21 +0700

    update

--- e_commerce/service/urls.py
+++ e_commerce/service/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 app_name = 'service'
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-<<<<<<< Updated upstream
+
     path('/register', views.register, name="register"),
     path('profile',views.profile, name='profile'),
-=======
+
     path('register', views.register, name="register"),
->>>>>>> Stashed changes
+
     path('shop', views.shop_page, name='shop_page'),
     path('cart', views.cart_page, name='cart_page')
 ]
